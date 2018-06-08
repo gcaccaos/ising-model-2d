eaa47c0d9d6870dad650c9d80736a7a95c46a61d
gcaccaos <gabriel.caccaos@gmail.com>
2018-06-08 02:23:35 -0300

    Criada a funcao para calcular a energia e mudado o codigo da magnetizacao.

--- codigos/ising2d.py
+++ codigos/ising2d.py
@@ -4,6 +4,28 @@ from numba import jit
 
 @jit
 def metropolis(L, N, T, J = 1, h = 0, kB = 1):
+    '''Simula configuracoes de spin atraves do algoritmo de Metropolis. Sao realizados passos de Monte Carlo sobre os spins, simulando a interacao com o banho termico. Esse processo e repetido N vezes.
+    
+    Parametros
+    ----------
+    L: int
+        Largura do grid de spins.
+    N: int
+        Numero de varreduras de Monte Carlo.
+    T: float
+        Temperatura do banho termico.
+    J: float
+        Constante que caracteriza a interacao entre dois spins vizinhos.
+    h: float
+        Constante que caracteriza a interacao dos spins com o campo magnetico externo.
+    kB: float
+        Constante de Boltzmann.
+    
+    Retorna
+    -------
+    ndarray
+        Array de N configuracoes de spin para o grid de tamanho L por L.'''
+    
     spins = choice([-1, +1], (L, L, N)) # gera uma configuracao aleatoria de spins
     
     # Faz N varreduras sobre o grid
@@ -12,7 +34,7 @@ def metropolis(L, N, T, J = 1, h = 0, kB = 1):
         ordemi = permutation(L)
         ordemj = permutation(L)
         
-        # Varredura sobre os spins e evolucao (flips aleatorios) devido a temperatura
+        # Varredura sobre os spins
         for i in ordemi:
             for j in ordemj:
                 # Calculo da energia para flipar o spin na posicao i
@@ -26,9 +48,17 @@ def metropolis(L, N, T, J = 1, h = 0, kB = 1):
                     Pflip = exp(- Eflip/(kB*T)) # probabilidade de flipar o spin na posicao i
                     if uniform(0, 1) < Pflip:
                         spins[i, j, n] = - spins[i, j, n]
+        # Fim de um passo de Monte Carlo (representa uma interacao dos spins com o banho termico)
     return spins
 
 @jit
-def H(spins, J, h):
-    H = 0
-    return H
+def H(spins, J = 1, h = 0):
+    '''Calcula a energia de uma dada configuracao de spins.'''
+    L = spins.shape[0]
+    E = 0
+    
+    for i in range(L):
+        for j in range(L):
+            E += - spins[i, j]*(J*(spins[(i - 1)%L, j] + spins[(i + 1)%L, j] +
+                                  spins[i, (j - 1)%L] + spins[i, (j + 1)%L]) + h)
+    return E
