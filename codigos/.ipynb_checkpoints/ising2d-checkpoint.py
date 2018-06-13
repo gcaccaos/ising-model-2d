f54872cb445f98485152d3bbb373fcbb979ef780
gcaccaos <gabriel.caccaos@gmail.com>
2018-06-12 21:46:55 -0300

    Refeita parte do codigo de simulacao 2D.

--- codigos/.ipynb_checkpoints/ising2d-checkpoint.py
+++ codigos/.ipynb_checkpoints/ising2d-checkpoint.py
@@ -1,9 +1,9 @@
-from numpy import exp
+from numpy import exp, zeros
 from numpy.random import choice, permutation, uniform
 from numba import jit
 
 @jit
-def metropolis(L, N, T, J = 1, h = 0, kB = 1):
+def ising2d(L, N, T, J = 1, h = 0, kB = 1):
     '''Simula configuracoes de spin atraves do algoritmo de Metropolis. Sao realizados passos de Monte Carlo sobre os spins, simulando a interacao com o banho termico. Esse processo e repetido N vezes.
     
     Parametros
@@ -26,7 +26,9 @@ def metropolis(L, N, T, J = 1, h = 0, kB = 1):
     ndarray
         Array de N configuracoes de spin para o grid de tamanho L por L.'''
     
-    spins = choice([-1, +1], (L, L, N)) # gera uma configuracao aleatoria de spins
+    spins = choice([-1, +1], (L, L, N)) # gera N configuracoes aleatorias de spins
+    M = zeros(N)
+    E = zeros(N)
     
     # Faz N varreduras sobre o grid
     for n in range(N):
@@ -38,27 +40,21 @@ def metropolis(L, N, T, J = 1, h = 0, kB = 1):
         for i in ordemi:
             for j in ordemj:
                 # Calculo da energia para flipar o spin na posicao i
-                Eflip = 2*spins[i, j, n]*(J*(spins[(i - 1)%L, j, n] + spins[(i + 1)%L, j, n] +
+                DeltaE = 2*spins[i, j, n]*(J*(spins[(i - 1)%L, j, n] + spins[(i + 1)%L, j, n] +
                                              spins[i, (j - 1)%L, n] + spins[i, (j + 1)%L, n]) + h)
 
                 # Se Eflip < 0, flipa o spin; caso contrario, aplica-se o passo de Monte Carlo
-                if Eflip < 0:
+                if DeltaE < 0:
                     spins[i, j, n] = - spins[i, j, n]
                 else:
-                    Pflip = exp(- Eflip/(kB*T)) # probabilidade de flipar o spin na posicao i
-                    if uniform(0, 1) < Pflip:
+                    PTrans = exp(- DeltaE/(kB*T)) # probabilidade de flipar o spin
+                    if uniform(0, 1) < PTrans:
                         spins[i, j, n] = - spins[i, j, n]
+                
+                # Atualiza a energia total da configuracao
+                E[n] -= DeltaE/2 # dividido por 2 para nao contar duas vezes cada spin
+                # Atualiza a magnetizacao total da configuracao
+                M[n] += spins[i, j, n]
+        
         # Fim de um passo de Monte Carlo (representa uma interacao dos spins com o banho termico)
-    return spins
-
-@jit
-def H(spins, J = 1, h = 0):
-    '''Calcula a energia de uma dada configuracao de spins.'''
-    L = spins.shape[0]
-    E = 0
-    
-    for i in range(L):
-        for j in range(L):
-            E += - spins[i, j]*(J*(spins[(i - 1)%L, j] + spins[(i + 1)%L, j] +
-                                  spins[i, (j - 1)%L] + spins[i, (j + 1)%L]) + h)
-    return E
+    return spins, M.mean(), E.mean()
