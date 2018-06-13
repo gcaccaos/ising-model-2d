ef13711687dc9bf02af161862ccd71e8cf479a32
gcaccaos <gabriel.caccaos@gmail.com>
2018-06-13 03:30:19 -0300

    Reformulado (quase totalmente) o codigo da simulacao 2D.

--- codigos/.ipynb_checkpoints/ising2d-checkpoint.py
+++ codigos/.ipynb_checkpoints/ising2d-checkpoint.py
@@ -3,58 +3,50 @@ from numpy.random import choice, permutation, uniform
 from numba import jit
 
 @jit
-def ising2d(L, N, T, J = 1, h = 0, kB = 1):
+def ising_update(spins, i, j, T, J, h):
+    kB = 1 # constante de Boltzmann (assim, a temperatura e dada em [T]/[kB])
+    L = spins.shape[0]
+    
+    # Calculo da energia para flipar o spin na posicao i, j
+    DeltaE = 2*spins[i, j]*(J*(spins[(i - 1)%L, j] + spins[(i + 1)%L, j] +
+                               spins[i, (j - 1)%L] + spins[i, (j + 1)%L]) + h)
+
+    # Se Eflip < 0, flipa o spin; caso contrario, aplica-se o passo de Monte Carlo
+    if DeltaE < 0:
+        spins[i, j] *= -1
+    else:
+        Pflip = exp(- DeltaE/(kB*T)) # probabilidade de flipar o spin
+        if uniform(0, 1) < Pflip:
+            spins[i, j] *= -1
+
+@jit
+def ising_step(spins, T, J, h):
     '''Simula configuracoes de spin atraves do algoritmo de Metropolis. Sao realizados passos de Monte Carlo sobre os spins, simulando a interacao com o banho termico. Esse processo e repetido N vezes.
     
     Parametros
     ----------
-    L: int
-        Largura do grid de spins.
-    N: int
-        Numero de varreduras de Monte Carlo.
+    spins: ndarray
+        Configuracao de spins.
     T: float
         Temperatura do banho termico.
     J: float
         Constante que caracteriza a interacao entre dois spins vizinhos.
     h: float
         Constante que caracteriza a interacao dos spins com o campo magnetico externo.
-    kB: float
-        Constante de Boltzmann.
     
     Retorna
     -------
     ndarray
         Array de N configuracoes de spin para o grid de tamanho L por L.'''
     
-    spins = choice([-1, +1], (L, L, N)) # gera N configuracoes aleatorias de spins
-    M = zeros(N)
-    E = zeros(N)
-    
-    # Faz N varreduras sobre o grid
-    for n in range(N):
-        # Ordem aleatoria para varrer os spins do grid
-        ordemi = permutation(L)
-        ordemj = permutation(L)
-        
-        # Varredura sobre os spins
-        for i in ordemi:
-            for j in ordemj:
-                # Calculo da energia para flipar o spin na posicao i
-                DeltaE = 2*spins[i, j, n]*(J*(spins[(i - 1)%L, j, n] + spins[(i + 1)%L, j, n] +
-                                             spins[i, (j - 1)%L, n] + spins[i, (j + 1)%L, n]) + h)
+    # Ordem aleatoria para varrer os spins do grid
+    L = spins.shape[0]
+    ordemi = permutation(L)
+    ordemj = permutation(L)
 
-                # Se Eflip < 0, flipa o spin; caso contrario, aplica-se o passo de Monte Carlo
-                if DeltaE < 0:
-                    spins[i, j, n] = - spins[i, j, n]
-                else:
-                    PTrans = exp(- DeltaE/(kB*T)) # probabilidade de flipar o spin
-                    if uniform(0, 1) < PTrans:
-                        spins[i, j, n] = - spins[i, j, n]
-                
-                # Atualiza a energia total da configuracao
-                E[n] -= DeltaE/2 # dividido por 2 para nao contar duas vezes cada spin
-                # Atualiza a magnetizacao total da configuracao
-                M[n] += spins[i, j, n]
-        
-        # Fim de um passo de Monte Carlo (representa uma interacao dos spins com o banho termico)
-    return spins, M.mean(), E.mean()
+    # Varredura sobre os spins
+    for i in ordemi:
+        for j in ordemj:
+            ising_update(spins, i, j, T, J, h)
+    
+    return spins
